c71b0871c8783c092499bbd1a02e925c7aa246d4
David Vos <vosdavid2@gmail.com>
2021-04-16 23:13:13 +0200

    More packages

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# Assuming Manjaro KDE
+
 sudo pacman -S yay
-yay -S base-devel latte-dock fish sublime-text-dev
+yay -S base-devel latte-dock fish neofetch \
+	sublime-text-dev pycharm-professional webstorm-jre datagrip \
+	ferdi discord_arch_electron spotify spotifywm-git
 
 git config --global user.email "vosdavid2@gmail.com"
 git config --global user.name "David Vos"
@@ -9,7 +13,13 @@ git config --global user.name "David Vos"
 chsh -s /bin/fish
 curl -L https://get.oh-my.fish | fish
 
-cp -r ./dot-config ~/.config
+omf install agnoster bass
+
+cp -r ./dot-config/* ~/.config
 
 echo "TODO: add ssh-keys for GitHub and GitLabs (don't forget to create ~/.ssh/config)
-https://docs.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent"
+https://docs.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent
+
+TODO: setup Plasma theme and keyboard shortcuts
+
+TODO: setup autostart and window rules"
